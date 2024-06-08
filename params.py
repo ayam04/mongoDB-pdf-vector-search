44d3e2441c13d9a8e1dd1627cee5aa350b09da2b
Ayam <ayamullahkhan04@gmail.com>
2024-06-08 20:31:34 +0530

    Update params.py

--- params.py
+++ params.py
@@ -1,7 +1,7 @@
 import urllib.parse as prs
 
-username = "ayam04"
-password = "1234@AbbuMamma"
+username = "username"
+password = "password"
 mongodb_conn_string = f"mongodb+srv://{username}:{prs.quote_plus(password)}@cluster0.hdcpwsw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&ssl=true"
 database = "test1"
 collection = "resumes"
